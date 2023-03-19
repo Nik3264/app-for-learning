600b39839dacdc6e975de9124aacfa096b4ed6ca
Oksana <example@gmail.com>
2023-03-19 19:11:36 +0200

    media requests

--- index.js
+++ index.js
@@ -9,64 +9,10 @@ import {getResource} from './src/getresource.js'
 //I have got the object from request and use it here,
 //becouse not all browsers works with this API
 
-/* const url =  'https://api.wisey.app/api/v1/core/preview-courses';
-const url1 = "https://api.wisey.app/api/v1/auth/anonymous?platform=subscriptions"; */
-const urlId=url+'/3b77ceb6-fb43-4cf5-a25b-8fe9222a0714';
-
-let arr;
-
-/* const getResource = async (url) => {
-    const res = await fetch(url);
-    return await res.json();
-  };
-
-const getResUrl = async (urlAuth, urlRes) => {
-  return await getResource(urlAuth).then(data=>{
-  let token=data.token;
-  console.log(token);
-  fetch(urlRes,{
-    method: "GET",
-    headers:{
-    'Content-type': 'application/json',
-    'Authorization': `Bearer ${token}`
-    }
-  }).then((response)=>{
-    return response.json();
-  }).then(data=>{
-    console.log('data  ',data);
-    return data;
-  })
-  .catch(()=>console.log('erRor'));
-  });
-}; */
-
-/* getResUrl(url1,url).then(data=>{
-  arr =  data;
-  console.log('arr  ',arr);
-}); */
-
-/* const getResource = async (urlAuth, url) => {
-  let res = await fetch(urlAuth);
-  let token = await res.json();
-
-  let resData = await fetch(url,{
-    method: "GET",
-    headers:{
-    'Content-type': 'application/json',
-    'Authorization': `Bearer ${token.token}`
-    }
-  });
-  let data = await resData.json();
-  console.log(data);
-  return data;
-}; */
-
 getResource(url1,url).then((data)=>{
   console.log('then data  ',data);
 
   const content = document.querySelector(".content");
-  //let dataImg=courseList.courses[1].previewImageLink+'/cover.webp';
-  //console.log(dataImg);
   let courses=data.courses;
   let cardCount=9;
   renderCard(courses, content, cardCount);
@@ -77,7 +23,6 @@ getResource(url1,url).then((data)=>{
       let idClicked;
       while (target.getAttribute("class") != "course-wrap" 
       && target.getAttribute("class") != "content"
-      && target.getAttribute("class") != "go-to-main"
       && target.getAttribute("class") != "button-more") {
           target = target.parentNode;
       } //while
@@ -89,18 +34,20 @@ getResource(url1,url).then((data)=>{
           renderPage(content,courses[idClicked]['id']);
           //window.location.href="page.html";
       }
-      if (target.getAttribute("class") === "go-to-main"){
-        const h1=document.querySelector('.h1');
-        h1.innerHTML=`Look, there's something for you here...`;
-        renderCard(courses, content,cardCount);
-      }
 
       if (target.getAttribute("class") === "button-more"){
         cardCount+=9;
         console.log(cardCount);
         renderCard(courses, content,cardCount);
       }
-      
+  });
+
+  const btnMain=document.querySelector('.go-to-main');
+  btnMain.addEventListener("click",()=>{
+    const h1=document.querySelector('.h1');
+    btnMain.classList.add('btn-up_hide');
+    h1.innerHTML=`Look, there's something for you here...`;
+    renderCard(courses, content,cardCount);
   });
 
   //button up
@@ -118,7 +65,3 @@ getResource(url1,url).then((data)=>{
     });
   };
 });//getResource(url1,url)
-
-
-
-
--- src/page.js
+++ src/page.js
@@ -4,12 +4,14 @@ import {url1} from './source.js';
 
 export default function renderPage(domContent, id) {
       let currentTime;
-  
+
+      const btnMain=document.querySelector('.go-to-main');
+      btnMain.classList.remove('btn-up_hide');
+
       domContent.innerHTML = "";
       domContent.innerHTML += `
     <div>
-      <button class="go-to-main">Go to main...</button>
-      <h3 class="page-description"></h3>
+      <h2 class="page-description"></h2>
       <p class="locked-lessons"></p>
       <script src="https://cdn.jsdelivr.net/npm/hls.js@1"></script>
       <ol class="lessons-list"><p>Loading...</p>
@@ -47,10 +49,6 @@ export default function renderPage(domContent, id) {
         else if (video.canPlayType('application/vnd.apple.mpegurl')) {
             video.src = videoSrc;
         }  
-
-/*         video.addEventListener("error",(e)=>{
-          console.log("error   ", e);
-        }); */
         
         video.addEventListener("pause",(e)=>{
           console.log("pause   ", e.target.currentTime);
@@ -58,39 +56,5 @@ export default function renderPage(domContent, id) {
           console.log(currentTime);
           localStorage.setItem(id,currentTime);
         });
-
-/*         lessonListHtml.addEventListener("click",(e)=>{
-          e.preventDefault();
-          let target = e.target;
-          if(target.getAttribute("class")==="unlocked"){
-            target.innerHTML=`
-              <div class="lesson-wrap">
-                <video id=${target.getAttribute("id")} class="video-lesson" controls></video>
-              </div>
-            `;
-          }
-
-        const video1 = document.getElementById(target.getAttribute("id"));
-        const videoSrc = data.lessons[target.getAttribute("id")].link;
-        console.log(videoSrc);
-        if (Hls.isSupported()) {
-            const start = 0;//+localStorage.getItem(id);
-            const hls = new Hls({capLevelToPlayerSize:true,
-              startPosition:start});
-            hls.loadSource(videoSrc);
-            hls.attachMedia(video1);
-            video1.play();
-        }
-        else if (video1.canPlayType('application/vnd.apple.mpegurl')) {
-            video1.src = videoSrc;
-        }  
-
-
-        }); *///lessonList
-
       });//getResource(url1,urlId)
   }
-
-
-
-
